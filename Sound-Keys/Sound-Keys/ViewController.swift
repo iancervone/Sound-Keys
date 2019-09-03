1fde1e0afe46ac79745dd5a33105d27052bbe904
Ian <ian@Ians-MacBook-Pro.local>
2019-09-03 17:32:03 -0400

    added keys to UI, added sound files to project

--- Sound-Keys/Sound-Keys/ViewController.swift
+++ Sound-Keys/Sound-Keys/ViewController.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+  var keySound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "note1", ofType: "wav")!)
+  var player: AVAudioPlayer?
+  
+  @IBAction func SoundKeyButton(_ sender: UIButton) {
+    let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+    do {
+      player = try AVAudioPlayer(contentsOf: url)
+      guard let player = player else { return }
+      
+      player.prepareToPlay()
+      player.play()
+    } catch let error as Error {
+      print(error)
+    }
+    
+  }
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
